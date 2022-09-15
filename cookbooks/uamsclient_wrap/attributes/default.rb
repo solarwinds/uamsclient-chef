9669acaba304689e3f48d74bc975c5a50739b1d3
Maciej Poleszczyk <maciej.poleszczyk@solarwinds.com>
2022-09-15 13:45:22 +0200

    Support for Windows

--- cookbooks/uamsclient_wrap/attributes/default.rb
+++ cookbooks/uamsclient_wrap/attributes/default.rb
@@ -1,2 +1,4 @@
 default['uamsclient']['uams_access_token'] = "xxxx"
 default['uamsclient']['uams_metadata'] = 'role:host-monitoring'
+# local_pkg_path has to be overriden for Windows
+# default['uamsclient']['local_pkg_path'] = 'C:\temporary'
