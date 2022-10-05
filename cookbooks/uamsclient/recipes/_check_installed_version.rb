a476f0ea933d83402497c4152e267f468ef6c56f
Maciej Poleszczyk <maciej.poleszczyk@solarwinds.com>
2022-10-05 21:40:57 +0200

    Fix

--- cookbooks/uamsclient/recipes/_check_installed_version.rb
+++ cookbooks/uamsclient/recipes/_check_installed_version.rb
@@ -2,14 +2,12 @@ ruby_block 'Evaluate command to get installed version' do
   block do
     if platform_family?('windows')
       command_if_uams_installed = '"C:\\Program Files\\SolarWinds\\UAMSClient\\uamsclient.exe" version'
-    else
-      if node.run_state['package_manager'] == 'apt'
-        command_if_uams_installed = "dpkg-query -l uamsclient | tail -n1 | awk '{print $3}' | cut -d- -f1"
-      elsif node.run_state['package_manager'] == 'yum'
-        command_if_uams_installed = "yum list | grep uamsclient | awk '{print $2}' | cut -f1 -d-"
-      elsif node.run_state['package_manager'] == 'dnf'
-        command_if_uams_installed = "dnf list --installed | grep uams | awk '{print $2}' | cut -f1 -d-"
-      end
+    elsif node.run_state['package_manager'] == 'apt'
+      command_if_uams_installed = "dpkg-query -l uamsclient | tail -n1 | awk '{print $3}' | cut -d- -f1"
+    elsif node.run_state['package_manager'] == 'yum'
+      command_if_uams_installed = "yum list | grep uamsclient | awk '{print $2}' | cut -f1 -d-"
+    elsif node.run_state['package_manager'] == 'dnf'
+      command_if_uams_installed = "dnf list --installed | grep uams | awk '{print $2}' | cut -f1 -d-"
     end
     node.run_state['command_if_uams_installed'] = command_if_uams_installed
   end
