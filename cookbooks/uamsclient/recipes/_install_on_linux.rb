c7188a133db840239941d561228c631331344e1c
Maciej Poleszczyk <maciej.poleszczyk@solarwinds.com>
2022-10-07 12:26:24 +0200

    NH-21721 Check installed and available versions (#9)

--- cookbooks/uamsclient/recipes/_install_on_linux.rb
+++ cookbooks/uamsclient/recipes/_install_on_linux.rb
@@ -1,11 +1,18 @@
 execute 'Install UAMS Client with apt' do
   command lazy { "apt install -y #{node.run_state['installation_pkg']}" }
   action :run
-  only_if { node.run_state['package_manager'] == 'apt' }
+  only_if { node.run_state['install_new_version'] && node.run_state['package_manager'] == 'apt' && !node['uamsclient']['dev_container_test'] }
+end
+
+execute 'Install UAMS Client with apt - in container' do
+  command lazy { "apt install -y #{node.run_state['installation_pkg']}" }
+  action :run
+  ignore_failure true
+  only_if { node.run_state['install_new_version'] && node.run_state['package_manager'] == 'apt' && node['uamsclient']['dev_container_test'] }
 end
 
 package 'Install UAMS Client with dnf/yum' do
   package_name lazy { "#{node.run_state['installation_pkg']}" }
   action :install
-  only_if { %w(dnf yum).include? node.run_state['package_manager'] }
+  only_if { node.run_state['install_new_version'] && (%w(dnf yum).include? node.run_state['package_manager']) }
 end
