1a024e8fcaa358ae17bcfbfa1fc2c9ce390aff38
Arkadiusz Nitka <arkadiusz.nitka@solarwinds.com>
2024-04-23 15:12:42 +0200

    Bump version

--- cookbooks/uamsclient_wrap/metadata.rb
+++ cookbooks/uamsclient_wrap/metadata.rb
@@ -3,7 +3,7 @@ maintainer 'SolarWinds'
 maintainer_email 'support@solarwinds.com'
 license 'MIT'
 description 'Example wrapper cookbook'
-version '0.6.0'
+version '0.7.0'
 chef_version '>= 16.0'
 
 depends 'uamsclient'
